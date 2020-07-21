8f10677029b2f837435585047f1b8acaaa62e890
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-07-21 13:01:59 +0300

    Added task1

--- week_1/task_1/index.js
+++ week_1/task_1/index.js
@@ -1,17 +1,5 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-});
-
 const getReversedString = ( line = '' ) => line.split('').reverse().join('');
 
-process.stdin.on( 'line', function (line) {
-    console.log(line)
+process.stdin.on( 'data', function (line) {
+    process.stdout.write(getReversedString(line.toString()));
 } )
-
-// rl.on('line', function(line = ''){
-//     process.stdout.write(getReversedString(line));
-// })
