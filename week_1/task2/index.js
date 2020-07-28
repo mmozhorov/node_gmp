1e9c06fab6bddc507c122e5f6c49e815d119ce7d
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-07-28 11:23:18 +0300

    Try to add pipeline

--- week_1/task2/index.js
+++ week_1/task2/index.js
@@ -1,40 +1,61 @@
+// const fs = require('fs');
+// const { pipeline, Duplex } = require('stream');
+// const { promisify } = require('util');
+// const path = require('path');
+// const csv = require('csvtojson');
+//
+// const pipelineAsync = promisify(pipeline);
+//
+// async function convertDataFromCSVString( chunk ) {
+//     return csv({ noheader: true, output: "csv" }).fromString(chunk);
+// }
+//
+// function generateJSONRow( headers, row ) {
+//     const result = {};
+//     for ( let i = 0; i < headers.length; i++ ){
+//         result[headers[i]] = row[i];
+//     }
+//     return JSON.stringify(result) + '\n';
+// }
+//
+// const readStream = new fs.createReadStream(path.resolve('./task2/csv/example.csv'), "utf8");
+// const writeStream = fs.createWriteStream(path.resolve('./task2/txt/result.txt'));
+// async function * generate(source) {
+//     for await (const chunk of source) {
+//         yield chunk.toUpperCase();
+//     }
+// }
+// const workingStream = Duplex.from(generate());
+//
+// async function run() {
+//     await pipelineAsync(
+//         readStream,
+//         workingStream,
+//         writeStream
+//     );
+//     console.log('Pipeline succeeded.');
+// }
+//
+// run().catch(console.error);
+
+const { pipeline } = require('stream');
 const fs = require('fs');
-const path = require('path');
-const csv = require('csvtojson');
 
-async function convertDataFromCSVString( chunk ) {
-    return csv({ noheader: true, output: "csv" }).fromString(chunk);
+const pipeline = util.promisify(stream.pipeline);
+const fs = require('fs');
+
+async function run() {
+    await pipeline(
+        fs.createReadStream('lowercase.txt'),
+        async function* (source) {
+            source.setEncoding('utf8');  // Work with strings rather than `Buffer`s.
+            for await (const chunk of source) {
+                yield chunk.toUpperCase();
+            }
+        },
+        fs.createWriteStream('uppercase.txt')
+    );
+    console.log('Pipeline succeeded.');
 }
 
-function generateJSONRow( headers, row ) {
-    const result = {};
-    for ( let i = 0; i < headers.length; i++ ){
-        result[headers[i]] = row[i];
-    }
-    return JSON.stringify(result) + '\n';
-}
-
-const readFilePath = path.resolve('./task2/csv/example.csv');
-const writeFilePath = path.resolve('./task2/txt/result.txt');
-
-const readStream = new fs.createReadStream(readFilePath, "utf8");
-const writeStream = fs.createWriteStream(writeFilePath);
-
-readStream.on("data", async function(chunk){
-    const [ headers, ...rows ] = await convertDataFromCSVString(chunk);
-    const resultJSON = rows.map( row => generateJSONRow( headers, row ));
-
-    writeStream.write(resultJSON.join(''));
-
-    writeStream.on('error', err => {
-        console.error(err);
-    });
-});
-
-readStream.on('error', err => {
-    console.error(err);
-});
-
-readStream.on('end', function () {
-    writeStream.end();
-});
+run().catch(console.error);
