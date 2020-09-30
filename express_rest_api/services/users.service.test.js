64ba4e9771336107f9f0cdfc4655e4625ccc1d69
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-09-30 16:41:50 +0300

    Try to add TS tests

--- express_rest_api/services/users.service.test.js
+++ express_rest_api/services/users.service.test.js
@@ -1,3 +1,18 @@
+import { Container } from "inversify";
+import { beforeAll } from "@jest/globals";
+
+import { DBInterface, DB } from '../types/db.types';
+import { PostgresqlTest } from '../loaders/postgresql-test';
+import {UsersService} from "./users.service";
+
+const serviceContainer = new Container();
+let UserServiceInstance;
+
+beforeAll(() => {
+    serviceContainer.bind<DBInterface>(DB).to(PostgresqlTest);
+    UserServiceInstance = new UsersService( serviceContainer.get<DBInterface>(DB) );
+});
+
 describe('My Test Suite', () => {
     it('My Test Case', () => {
         expect(true).toEqual(true);
